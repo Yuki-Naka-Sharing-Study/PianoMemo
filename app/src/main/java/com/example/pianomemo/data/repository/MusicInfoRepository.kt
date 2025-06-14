3bc8d24c909f1bb71f363125facb31147061afbe
仲優樹 <yu-ki216m@outlook.jp>
2025-06-14 14:04:30 +0900

    Created MusicInfoRepository class.

--- app/src/main/java/com/example/pianomemo/data/repository/MusicInfoRepository.kt
+++ app/src/main/java/com/example/pianomemo/data/repository/MusicInfoRepository.kt
@@ -1,2 +1,36 @@
 package com.example.pianomemo.data.repository
 
+import com.example.pianomemo.data.local.MusicInfo
+import com.example.pianomemo.data.local.MusicInfoDao
+
+class MusicInfoRepository(private val musicInfoDao: MusicInfoDao) {
+    suspend fun saveMusicInfo(
+        textOfMusic: String,
+        textOfArtist: String,
+        textOfGenre: String,
+        textOfStyle: String,
+        textOfMemo: String,
+        numOfRightHand: Float,
+        numOfLeftHand: Float
+    ){
+        val musicInfo = MusicInfo(
+            nameOfMusic = textOfMusic,
+            nameOfArtist = textOfArtist,
+            nameOfJenre = textOfGenre,
+            nameOfStyle = textOfStyle,
+            nameOfMemo = textOfMemo,
+            levelOfRightHand = numOfRightHand.toInt(),
+            levelOfLeftHand = numOfLeftHand.toInt()
+        )
+        musicInfoDao.insertMusicInfo(musicInfo)
+    }
+    suspend fun getAllMusicInfo(): List<MusicInfo> {
+        return musicInfoDao.getAllMusicInfo()
+    }
+    suspend fun deleteMusicInfo(musicInfo: MusicInfo) {
+        musicInfoDao.deleteMusicInfo(musicInfo)
+    }
+    suspend fun updateMusicInfo(musicInfo: MusicInfo) {
+        musicInfoDao.updateMusicInfo(musicInfo)
+    }
+}
