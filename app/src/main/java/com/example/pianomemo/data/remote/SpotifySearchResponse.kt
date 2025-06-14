15df7fc4ccbccca88a9ed5e20f9702b52af2783d
仲優樹 <yu-ki216m@outlook.jp>
2025-06-14 14:00:05 +0900

    Created SpotifySearchResponse data class.

--- app/src/main/java/com/example/pianomemo/data/remote/SpotifySearchResponse.kt
+++ app/src/main/java/com/example/pianomemo/data/remote/SpotifySearchResponse.kt
@@ -1,2 +1,23 @@
 package com.example.pianomemo.data.remote
 
+data class SpotifySearchResponse(
+    val tracks: SpotifyTracks,
+    val artists: SpotifyArtists
+)
+
+data class SpotifyTracks(
+    val items: List<Track>
+)
+
+data class SpotifyArtists(
+    val items: List<Artist>
+)
+
+data class Track(
+    val name: String,
+    val artists: List<Artist>
+)
+
+data class Artist(
+    val name: String
+)
