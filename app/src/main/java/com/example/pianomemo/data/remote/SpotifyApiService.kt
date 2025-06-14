fb7de0b2944d4b3704e51803a917d8bec769d6a5
仲優樹 <yu-ki216m@outlook.jp>
2025-06-14 14:01:34 +0900

    Created RetrofitInstance object and SpotifyApiService interface.

--- app/src/main/java/com/example/pianomemo/data/remote/SpotifyApiService.kt
+++ app/src/main/java/com/example/pianomemo/data/remote/SpotifyApiService.kt
@@ -1,2 +1,16 @@
 package com.example.pianomemo.data.remote
 
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Query
+
+interface SpotifyApiService {
+    @GET("v1/search")
+    suspend fun searchMusic(
+        @Query("q") query: String,
+        @Query("type") type: String = "track,artist",
+        @Query("limit") limit: String = "5",
+        @Header("Authorization") authHeader: String
+    ): Response<SpotifySearchResponse>
+}
