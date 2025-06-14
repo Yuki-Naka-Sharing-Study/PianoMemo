498b023c22d6a5a955a0d4ed987cb24f456497f4
仲優樹 <yu-ki216m@outlook.jp>
2025-06-14 13:50:42 +0900

    Created MusicInfo data class.

--- app/src/main/java/com/example/pianomemo/data/local/MusicInfo.kt
+++ app/src/main/java/com/example/pianomemo/data/local/MusicInfo.kt
@@ -1,2 +1,16 @@
 package com.example.pianomemo.data.local
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "music_info")
+data class MusicInfo(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    val nameOfMusic: String,
+    val nameOfArtist: String,
+    val nameOfJenre: String,
+    val nameOfStyle: String,
+    val nameOfMemo: String,
+    val levelOfRightHand: Int,
+    val levelOfLeftHand: Int,
+)
